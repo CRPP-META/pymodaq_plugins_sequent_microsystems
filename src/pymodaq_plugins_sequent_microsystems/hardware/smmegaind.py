6c8e368168687c987a76d94f623ce2fcde475bbf
Bastien Bégon <bastien.begon@crpp.cnrs.fr>
2025-09-17 15:01:18 +0200

    Start creation of wrapper for industrial automation board

--- src/pymodaq_plugins_sequent_microsystems/hardware/smmegaind.py
+++ src/pymodaq_plugins_sequent_microsystems/hardware/smmegaind.py
@@ -9,8 +9,35 @@ import megaind
 
 class SMMegaInd:
     """
-    Sequent Microsystems Industrial Automation Hat class
+    Sequent Microsystems Industrial Automation Hat wrapper class
     """
+    def __init__(self, stack):
+        self.open_connection(stack)
 
-    def __init__(self):
-        pass
+    def open_connection(self, stack):
+        self.stack = stack
+        for channel in range(1, 5):
+            megaind.set0_10Out(self.stack, channel, 0)
+
+    def close_connection(self):
+        for channel in range(1, 5):
+            megaind.set0_10Out(self.stack, channel, 0)
+    
+    def check_connection(self):
+        return megaind.checkStack(self.stack)
+    
+    def get0_10Out(self, channel):
+        return megaind.get0_10Out(self.stack, channel)
+    
+    def set0_10Out(self, channel, value):
+        return megaind.set0_10Out(self.stack, channel, value)
+    
+    def set0_10Out_rel(self, channel, value):
+        V_old = megaind.get0_10Out(self.stack, channel)
+        return megaind.set0_10Out(self.stack, channel, V_old + value)
+
+if __name__ == "__main__":
+    smmegaind = SMMegaInd(0)
+
+    smmegaind.set0_10Out(1, 0)
+    print(smmegaind.get0_10Out(1))
