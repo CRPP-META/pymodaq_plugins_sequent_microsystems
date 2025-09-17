2fb1b0a76675e1fb98821550a0924040c9f1e8ac
Bastien Bégon <bastien.begon@crpp.cnrs.fr>
2025-09-17 02:37:42 +0200

    Create industrial auto hat python wrapper

---
+++ src/pymodaq_plugins_sequent_microsystems/hardware/smmegaind.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 17 18:19:19 2025
+
+@author: Bastien Bégon
+"""
+
+import megaind
+
+class SMMegaInd:
+    """
+    Sequent Microsystems Industrial Automation Hat class
+    """
+
+    def __init__(self):
+        pass
